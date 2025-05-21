7ca194c1b0cc0dfb3037b1542de5afa3fd961120
campbell26 <cam24018@byui.edu>
2025-05-21 00:57:01 -0700

    blog html and css

---
+++ book-review-website/blog.js
@@ -0,0 +1,22 @@
+const books = [
+  {
+    title: "Septimus Heap Book One: Magyk",
+    author: "Angie Sage",
+    published: "July 5, 2022",
+    age: "10-14",
+    genre: "Fantasy",
+    rating: 4,
+    cover: "images/Magyk.jpeg",
+    description: "If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.",
+  },
+  {
+    title: "Magnus Chase Book One: Sword of Summer",
+    author: "Rick Riordan",
+    published: "December 12, 2021",
+    age: "12-16",
+    genre: "Fantasy",
+    rating: 5,
+    cover: "images/sword-of-summer.jpg",
+    description: "If you like Norse mythology and fast-paced action, this is the book for you.",
+  }
+];
